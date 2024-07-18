9864506cc646226e540799284c9a3d82efb8bfbc
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-19 02:48:41 +0900

    for()

---
+++ 15.for().c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+    //초기식; 조건식; 증감식;
+    //조건식은 매 반복에 앞서서 진행
+    //증감식은 매 반복 실행 후 마지막에 진행
+    for(int i = 0; i < 5; i++){
+        printf("Hello World\n");
+    }
+
+    for (int i = 1; i < 10; i++)
+    {   
+        for (int j = 1; j < 10; j++)
+    {   
+        printf("%d * %d = %d\n",i,j,i*j);
+    }
+    }
+    
+    return 0;
+}
