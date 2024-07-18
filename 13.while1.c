42d906c23515733c082b83f0aa88ad6bd532ef00
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-19 02:33:50 +0900

    while

--- 13.while1.c
+++ 13.while1.c
@@ -8,11 +8,23 @@ int main(void){
         printf("Hello Wold %d\n",num++);
     }
     
-    while (1)//false(0)이 아닌 값을 집어 넣으면 무한 반복 됨.
+    // while (1)//false(0)이 아닌 값을 집어 넣으면 무한 반복 됨.
+    // {
+    //     printf("료이키 텐카이\n");
+    // }
+
+    int c1 = 0;
+    while (c1 < 5)
     {
-        printf("료이키 텐카이\n");
+        int c2 = 0;
+        while (c2 < c1)
+        {
+            printf("O ");
+            c2++;
+        }
+        c1++;
+        printf(" *\n");
     }
     
-
     return 0;
 }
