0802b1950bb847afe5bee350a3609ffb32aabc20
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-19 02:28:38 +0900

    scanf()와 서식문자

--- 6.scanf2.c
+++ 6.scanf2.c
@@ -12,5 +12,7 @@ int main(void) {
     //float: %f, double: %lf, long double: %Lf
     //printf()로 출력할 때의 서식문자와 좀 헷갈린다. printf()와 scanf()의 서식문자는 별도로 이해하고 있어야 한다.
 
+    //추가로 서식문자 %s는 문자열의 입력에 사용된다.
+
     return 0;
 }
