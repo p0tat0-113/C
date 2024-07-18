8b70f1db999f86c859eeaad7b3701c8b7af54610
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-19 02:20:11 +0900

    scanf()와 서식문자

---
+++ 6.scanf2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1;
+    int num2;
+    int num3;
+    printf("10진수 8진수 16진수 입력: ");
+    scanf("%d %o %x",&num1,&num2,&num3); //12 12 12
+    printf("%d %d %d\n",num1,num2,num3); //12 10 18
+
+    //실수 형태의 입력 형태 정의
+    //float: %f, double: %lf, long double: %Lf
+    //printf()로 출력할 때의 서식문자와 좀 헷갈린다. printf()와 scanf()의 서식문자는 별도로 이해하고 있어야 한다.
+
+    return 0;
+}
