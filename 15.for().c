b00c8c1360cd96440bd5217afede451dff39d9b1
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-19 03:13:09 +0900

    for()

--- 15.for().c
+++ 15.for().c
@@ -9,12 +9,10 @@ int main(void) {
         printf("Hello World\n");
     }
 
-    for (int i = 1; i < 10; i++)
-    {   
-        for (int j = 1; j < 10; j++)
-    {   
+    for (int i = 1; i < 10; i++){   
+        for (int j = 1; j < 10; j++){   
         printf("%d * %d = %d\n",i,j,i*j);
-    }
+        }   
     }
     
     return 0;
